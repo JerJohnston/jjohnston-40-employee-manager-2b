322bdece6c9966382dad011dc3f15c85f980cc51
JerJohnston <59575963+JerJohnston@users.noreply.github.com>
2021-03-13 12:58:48 -0700

    refresh app

--- client/js/dashboard/sidebar/sideBarOptions.js
+++
@@ -1,9 +0,0 @@
- 
- import {optionsTemplate} from './sideBarTemplates'
- import renderToElement from '../../utils/renderToElement'
-
- const sideBarOptions = function(){
-      return renderToElement(optionsTemplate)
- }
- 
- export default sideBarOptions 
